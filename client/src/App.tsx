523f10ab19778ab968e914829f04a17ea97438d3
Shashank Gavale <shashankmgavale2004@gmail.com>
2025-09-23 12:27:13 +0530

    Frontend Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-300'>
+        <Button>Click</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
