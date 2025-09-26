dad3ab2c9b6e8dab6169dc890a3695ad545e651c
Shashank Gavale <shashankmgavale2004@gmail.com>
2025-09-26 18:46:14 +0530

    bug Fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,41 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"
 import Navbar from './components/ui/Navbar'
 import Home from './pages/Home'
+ // example page
 import { ThemeProvider } from './components/theme/ThemeProvider'
 import { useTheme } from './components/theme/ThemeProvider'
 import Footer from '@/components/ui/Footer'
+import Auth from "./pages/Auth"
+
 function App() {
   return (
     <ThemeProvider>
-      <div className="min-h-screen bg-background text-foreground flex flex-col">
-        <ThemedNavbarWrapper />
-        <main className="flex-1 pt-8 pb-24">{/* spacing for bottom navbar and footer */}
-          {/* <Auth/>  */}
-          <Home />
-        </main>
-        <Footer />
-      </div>
+      <Router>
+        <AppShell />
+      </Router>
     </ThemeProvider>
+  )
+}
+
+function AppShell() {
+  const location = useLocation()
+  const pathname = location.pathname.toLowerCase()
+  const isAuthRoute = pathname === "/login" || pathname === "/auth"
+
+  return (
+    <div className="min-h-screen bg-background text-foreground flex flex-col">
+      {!isAuthRoute && <ThemedNavbarWrapper />}
+
+      <main className={isAuthRoute ? "flex-1" : "flex-1 pt-8 pb-24"}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/login" element={<Auth />} />
+        </Routes>
+      </main>
+
+      {!isAuthRoute && <Footer />}
+    </div>
   )
 }
 
